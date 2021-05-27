049dff154ecd4e28fbe0eaf7156b52265e2cc384
RjabChaalia <rejeb.chaalia@gmail.com>
2021-05-27 13:22:16 +0100

    add email field

--- backend/idea/models.py
+++ backend/idea/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from import_export.admin import ImportExportModelAdmin
 from datetime import date
 
+
 # Create your models here.
 class Idea(models.Model):
     id = models.AutoField(primary_key=True)
@@ -21,9 +22,14 @@ class Idea(models.Model):
     date = models.DateField(
         auto_now_add=True,
     )
+    email = models.EmailField(
+        null=False,
+        blank=False,
+        default=""
+    )
 
     def __str__(self):
-        return (self.title)
+        return self.title
 
     def to_json(self):
         return {
